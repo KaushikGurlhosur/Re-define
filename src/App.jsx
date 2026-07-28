950cae935fefc09a8559064052119152987511d3
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2026-07-28 19:52:47 +0530

    first commit - after cleanup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="flex h-screen justify-center items-center bg-black/80">
+      <h1 className="text-white font-black text-5xl tracking-widest">
+        ReDefine
+      </h1>
+    </div>
+  );
+};
+
+export default App;
