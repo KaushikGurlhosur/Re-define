224a2814dded9335eb6c79206a860cad66b8eb21
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2026-07-29 21:05:33 +0530

    feat: add global font faces and theme variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div className="flex h-screen justify-center items-center bg-black/80">
-      <h1 className="text-white font-black text-5xl tracking-widest">
+      <h1 className="font-black text-5xl tracking-widest text-white/80">
         ReDefine
       </h1>
     </div>
